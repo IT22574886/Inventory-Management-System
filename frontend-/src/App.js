500d2980b4e01136d1a8590ea20c52ec87889656
Lakshan H.P.A.T <it22574886@my.sliit.lk>
2025-07-12 23:32:10 +0530

    Update App.js
    remove routes no need

--- frontend-/src/App.js
+++ frontend-/src/App.js
@@ -84,14 +84,6 @@ function App() {
               </ProtectedRoute>
             }
           />
-           <Route
-            path="/admin/dashboard"
-            element={
-              <ProtectedRoute requireAuth={true} requireAdmin={true}>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
         </Routes>
       </React.Fragment>
     </AuthProvider>
